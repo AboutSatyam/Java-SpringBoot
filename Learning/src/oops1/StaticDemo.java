9f4b01a5d1ec82e7c087fc502d8140de9ddf8d2c
AboutSatyam <ProgrammerSatyam07@gmail.com>
2025-12-06 15:58:35 +0530

    6dec'

--- Learning/src/oops1/StaticDemo.java
+++ Learning/src/oops1/StaticDemo.java
@@ -5,21 +5,27 @@ class Demo1 {
 
 	public static void disp() {
 		int a = 12;
-		System.out.println(a);
+		System.out.println(a + "parent");
 	}
 
 }
 
 class Demo2 extends Demo1 {
+	public static void disp() {
+		int a = 12;
+		System.out.println(a + "child");
 
+	}
 }
 
 public class StaticDemo {
 
 	public static void main(String[] args) {
 
-		Demo2 d2 = new Demo2();
+		Demo1 d2 = new Demo2();
 		d2.disp();
 		Demo2.disp();
+		System.out.println("************");
+		Demo1.disp();
 	}
 }
